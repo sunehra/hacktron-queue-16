76d60b520b5dd1cc0548a04792b7facef7b0ed62
vdmehta36 <vdmehta36@gmail.com>
2019-08-10 11:19:10 +0530

    initial commit

---
+++ dbsQueue/src/main/java/com/dbs/hacktron/DbsQueueApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.hacktron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsQueueApplication.class, args);
+	}
+
+}
---
+++ dbsQueue/src/test/java/com/dbs/hacktron/DbsQueueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbs.hacktron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbsQueueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
